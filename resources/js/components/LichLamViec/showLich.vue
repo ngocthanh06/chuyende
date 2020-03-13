a894868db079291f0a8594ef32906c2e805136aa
hong loan <nthloan102@gmail.com>
2020-03-13 17:11:00 +0700

    getLichNv

--- resources/js/components/LichLamViec/showLich.vue
+++ resources/js/components/LichLamViec/showLich.vue
@@ -64,39 +64,28 @@
                   <!-- số ngày 7 -->
                   <div v-for="(item, dex) in getDatecaLam" :key="dex" class="i_desc titleTable">
                         <div class="checkElement" v-if="calam.workshifts != ''">
-                            <div  v-for="(c1, vk1) in calam.workshifts" :key="vk1" >
-                                <span class="abc" v-if="c1.WS_date === item">
-                                    {{dex}}
-                                </span>
-                            </div>
+                            <button role="button" @click="detailsCalam(calam.workshifts, item)" class="btn btn-pill btn-outline-success" href="#detail" data-toggle="modal" data-target="#detail">
+                                  <div class="ElementsWork" v-for="(c1, vk1) in calam.workshifts" :key="vk1" >
+                                      <svg  v-if="c1.WS_date === item"  style="color:rgb(0,0,0)" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-check">
+                                        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                                        <circle cx="8.5" cy="7" r="4"></circle>
+                                        <polyline points="17 11 19 13 23 9"></polyline>
+                                      </svg>
+                                  </div>
+                              </button>
                         </div>
 
-
-
-
-
-                      <!-- <div  class="checkElement"  v-if="calam.workshifts != ''"> -->
-                                       <!-- <template  v-for="(c1, vk1) in calam.workshifts">   -->
-                                          <!-- <div :key="vk1" > -->
-                                              <!-- <span v-if="c1.WS_date === item">
-                                                a
-                                              </span> -->
-                                              <!-- {{ length(calam.workshifts,item) }}  -->
-                                          <!-- </div>   -->
-                                      <!-- </template>     -->
-
-                      <!-- </div> -->
-                      <!-- <button v-else data-toggle="modal" data-target="#myModal" type="button" class="add_btn">
-                              <i aria-label="icon: plus" class="anticon anticon-plus">
-                                  <svg viewBox="64 64 896 896" focusable="false" data-icon="plus" width="1em" height="1em" fill="currentColor" aria-hidden="true" >
-                                    <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z" />
-                                    <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z" />
-                                  </svg>
-                                </i>
-                       </button> -->
+                      <button v-else type="button" class="add_btn" >
+                          <i aria-label="icon: plus" class="anticon anticon-plus">
+                              <svg viewBox="64 64 896 896" focusable="false" data-icon="plus" width="1em" height="1em" fill="currentColor" aria-hidden="true" >
+                                  <path d="M482 152h60q8 0 8 8v704q0 8-8 8h-60q-8 0-8-8V160q0-8 8-8z" />
+                                  <path d="M176 474h672q8 0 8 8v60q0 8-8 8H176q-8 0-8-8v-60q0-8 8-8z" />
+                              </svg>
+                          </i>
+                      </button>
                   </div>
                   <!-- Lịch -->
-                  <!-- <detailLich v-bind:getsInv="infoCaNv"></detailLich> -->
+                   <detailLich v-bind:getsInv="infoCaNv"></detailLich>
                 </div>
 
               </div>
@@ -145,7 +134,6 @@ export default {
       company: "",
       ValueCaLam: [],
       infoCaNv : [],
-      numsUser: [],
     };
   },
   created() {
@@ -195,52 +183,28 @@ export default {
       return (this.numWeek = moment().week());
     },
 
-    // get employer in company
-    getEmpCom() {
-      return this.$store.getters.getEmployerComp;
-    },
+
     // change employer when change choose company orther
     changeEmpComp() {
-
       this.loading = true;
       this.$store.dispatch('SetDateCaLam', [this.numWeek, this.numYear]);
       axios.post('/api/getsNgayLvNv',{idComp: this.company, date: this.getDatecaLam}).then(res => {
           this.ValueCaLam = res.data;
-          this.kiemtraCalam();
       }).catch();
-
-
     },
 
-    diLam(val){
-      this.infoCaNv = val;
-    },
-    kiemtraCalam() {
-        let countsWorkshifts = []
-        let arrWorkshifts = []
-        // get workshifts
-        this.ValueCaLam.forEach(response=>{
-            if(response.workshifts.length > 0){
-                // get detail workshifts
-                response.workshifts.forEach(res => {
-                    arrWorkshifts.push(res);
-                })
+    detailsCalam(val, date){
+        let value =[];
+        val.forEach(response => {
+            if(response.WS_date === date){
+                value.push(response);
             }
-        });
+        })
+        this.infoCaNv = value;
+    },
 
-        //Days for week
-        this.getDatecaLam.forEach(response=>{
-            let num = 0;
-            arrWorkshifts.forEach(res => {
-                if(response === res.WS_date)
-                num ++;
-            });
-            console.log(num);
-        });
+    kiemtraCalam(val, date) {
 
-
-        // console.log(arrWorkshifts)
-        // this.numsUser = countsWorkshifts;
     },
 
 
@@ -250,9 +214,6 @@ export default {
     // get all company
     getCompanies() {
       return this.$store.getters.getCompany;
-    },
-    getCaLam() {
-      return this.$store.getters.getCaLam;
     },
     ChangeCaLam() {
       return this.ValueCaLam;
@@ -272,4 +233,8 @@ export default {
   #getLichNv >>> .titleTable:first-child{border-left: 1px solid #e3e3e3; }
   #getLichNv >>> .titleTable .btn-dangky{border-color: #5b8c85; color: #5b8c85; width: 100%}
   #getLichNv >>> .titleTableSearch{background: #5b8c85; border-color: #000; color: #fff}
+  #getLichNv >>> .titleTable .checkElement{width: 100%}
+  #getLichNv >>> .titleTable .checkElement .btn-outline-success {width: 100%; display: block; text-align: center; border-color: none; border: none}
+  #getLichNv >>> .titleTable .checkElement .btn-outline-success:hover{background: #38c172}
+  #getLichNv >>> .titleTable .checkElement .ElementsWork{display: inline-block}
 </style>

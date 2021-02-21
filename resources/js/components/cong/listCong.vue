f4177ba8cfeaac8e92999d7e2e48ce0fc4bd0259
ngocthanh06 <ngocthanh6398@gmail.com>
2021-02-21 21:28:44 +0700

    coding convention bang luong thang list cong thong ke

--- resources/js/components/cong/listCong.vue
+++ resources/js/components/cong/listCong.vue
@@ -1,54 +1,62 @@
 <template>
-<section class="calendar">
-  <div class="card">
-    <div class="card-body">
-      <div class="list-view">
-        <!-- end title -->
-        <div class="content">
-          <el-tabs v-model="activeName" @tab-click="handleClick">
-            <el-tab-pane label="Thống kê công" name="thongke">
-              <thongke ref="thongke"></thongke>
-            </el-tab-pane>
-            <el-tab-pane label="Danh sách nhân viên tạm ứng" name="listTamung">
-              <listTamung ref="listTamung"></listTamung>
-            </el-tab-pane>
-            <el-tab-pane label="Lương" name="luong">
-              <luong ref="luong"></luong>
-            </el-tab-pane>
-          </el-tabs>
+  <section class="calendar">
+    <div class="card">
+      <div class="card-body">
+        <div class="list-view">
+          <!-- end title -->
+          <div class="content">
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+              <el-tab-pane label="Thống kê công" name="thongke">
+                <thongke ref="thongke"></thongke>
+              </el-tab-pane>
+              <el-tab-pane label="Danh sách nhân viên tạm ứng" name="listTamung">
+                <listTamung ref="listTamung"></listTamung>
+              </el-tab-pane>
+              <el-tab-pane label="Lương" name="luong">
+                <luong ref="luong"></luong>
+              </el-tab-pane>
+            </el-tabs>
+          </div>
         </div>
+        <!-- end list view -->
       </div>
-      <!-- end list view -->
+      <!-- end card-body -->
     </div>
-    <!-- end card-body -->
-  </div>
-  <!-- end card -->
-</section>
+    <!-- end card -->
+  </section>
 </template>
 
 <script>
+
 import luong from '../luong/listLuongNV';
 import listTamung from './listTamung';
 import thongke from './thongke';
+
 export default {
   components: {
     thongke,
     listTamung,
     luong
   },
+
   data() {
     return {
       activeName: 'first'
     }
   },
+
   methods: {
-    handleClick(tab, event) {
+    
+    handleClick(tab) {
+
       if(tab.name == 'thongke'){
         this.$refs.thongke.loadVal();
       }
+      
       if (tab.name == 'listTamung') {
         this.$refs.listTamung.loadVal();
       }
+      
       if(tab.name =='luong'){
         this.$refs.luong.allCompanies;
         this.$refs.luong.changeEmpComp();
--- resources/js/filters/index.js
+++ resources/js/filters/index.js
@@ -1,6 +1,12 @@
+import Vue from 'vue';
 import moment from 'moment';
+
 export default function formatDay(time) {
     if (time) {
         return moment(String(time)).format('DD');
     }
 }
+
+Vue.filter('formatMoney', function(monney) {
+    return monney.toLocaleString('it-IT', {style: 'currency', currency: 'VND' });
+});
